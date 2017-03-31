44934816afb96ba101c08942be6efd0cfb23c3d9
haondt <burghardtnoah@gmail.com>
2017-03-30 18:26:15 -0600

    Add files via upload

---
+++ sailfishv3/sailfishv3.ino
@@ -0,0 +1,42 @@
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(3,2);
+Servo esc;
+int hall = 6;
+int spd = 0;
+int i = 0;
+int maxAccel = 2;
+int maxspd =130;
+int minspd = 55;
+void setup(){
+  esc.attach(5);
+  esc.writeMicroseconds(700);
+  delay(2000);
+  esc.writeMicroseconds(2000);
+  delay(1000);
+  esc.writeMicroseconds(700);
+  delay(2000);
+  mySerial.begin(9600);
+  pinMode(6,INPUT);
+  digitalWrite(6,HIGH);
+  Serial.begin(9600);
+}
+void loop(){
+  if(mySerial.available()){
+    i = mySerial.parseInt();
+    if(i<=500){
+      if(spd<minspd){spd=minspd;}
+      spd+=map(i,500,0,1,maxAccel);
+      if(spd>maxspd){spd=maxspd;}
+    }
+    else if(i>=520){
+      spd-=map(1023-i,520,0,1,maxAccel);
+      if(spd<minspd){spd=0;}
+    }
+    esc.write(spd);
+    Serial.println(spd);
+  }
+} 
+
+
